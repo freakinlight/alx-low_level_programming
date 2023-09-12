e33d4e7c72376be037d07be8def64cc5d2f21130
freakinlight <mazouznour0@gmail.com>
2023-09-12 12:31:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Alway 0 (Sucess)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
 {
-	char *h = "Holberton\n";
-
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
