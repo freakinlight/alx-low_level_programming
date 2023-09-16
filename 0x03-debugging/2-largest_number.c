6630eb8f80066714b7e682fc8d2f55640b92e8d6
freakinlight <mazouznour0@gmail.com>
2023-09-15 17:34:42 -0700

    Fix largest_number function logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a >= b && a >= c)  // Changed ">" to ">=" to handle equal values
+    if ((a >= b && a >= c) || (a >= c && c >= b))
     {
         largest = a;
     }
-    else if (b >= a && b >= c)  // Changed ">" to ">=" to handle equal values
+    else if ((b >= a && b >= c) || (b >= c && c >= a))
     {
         largest = b;
     }
