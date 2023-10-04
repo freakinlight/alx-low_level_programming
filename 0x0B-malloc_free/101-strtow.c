3f33dda2e3c95eef78993dd340c849fc9521e9e3
freakinlight <mazouznour0@gmail.com>
2023-10-04 16:17:25 -0700

    Write a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 #include <stdlib.h>
 
 int word_len(char *str);
@@ -6,9 +6,9 @@ int count_words(char *str);
 char **strtow(char *str);
 
 /**
- * word_len - Locates the index marking the end of the 
+ * word_len - Locates the index marking the end of the
  *            first word contained within a string.
- * @str: The string to be searched. 
+ * @str: The string to be searched.
  *
  * Return: The index marking the end of the initial word pointed to by str.
  */
@@ -27,10 +27,10 @@ int word_len(char *str)
 }
 
 /**
- * count_words - Counts the number of words contained within a string. 
- * @str: The string is to be searched. 
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string is to be searched.
  *
- * Return: The number of words contained within str. 
+ * Return: The number of words contained within str.
  */
 
 int count_words(char *str)
@@ -55,7 +55,7 @@ int count_words(char *str)
 /**
  * strtow - Splits a string into words.
  * @str: The string to be split.
- * 
+ *
  * Return: If str = NULL, str = "", or the function fails - NULL.
  *         Otherwise - a pointer to an array of strings(words).
  */
